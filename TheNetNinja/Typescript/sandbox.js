286894c66028a725ab02c83a2c91d5ee51c32c3e
Nidhi Verma <nidhi99verma@gmail.com>
2021-07-16 15:00:29 +0530

    Delete TheNetNinja/Typescript directory

--- TheNetNinja/Typescript/sandbox.js
+++
@@ -1,5 +0,0 @@
-var inputs = document.querySelectorAll('input');
-//console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
--- TheNetNinja/Typescript/sandbox.ts
+++
@@ -1,8 +0,0 @@
-const inputs = document.querySelectorAll('input');
-
-//console.log(inputs);
-
-inputs.forEach(input => {
-    console.log(input);
-    
-})
